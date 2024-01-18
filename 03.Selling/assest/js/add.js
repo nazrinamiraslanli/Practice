d7d3fc5a99fd0520caa68e75484d7e9423ca7501
nazrinamiraslanli <nazrinamiraslanli38@gmail.com>
2024-01-18 09:28:00 +0400

    practice

--- 03.Selling/assest/js/add.js
+++ 03.Selling/assest/js/add.js
@@ -1,32 +1,50 @@
 const BASE_URL = "http://localhost:3000";
 const tBody = document.querySelector("tbody");
+const sort = document.querySelector(".sort");
+let product;
+let copyProduct;
 async function getData() {
-    const res = await axios(`${BASE_URL}/cards`);
-    console.log(res.data);
-    drawTable(res.data);
-  }
-  getData();
-  function drawTable(arr) {
-    tBody.innerHTML = "";
-    arr.forEach((element) => {
-      let tr = document.createElement("tr");
-      tr.innerHTML += `
+  const res = await axios(`${BASE_URL}/cards`);
+  console.log(res.data);
+  product = res.data;
+  copyProduct = [...res.data];
+  drawTable(res.data);
+  console.log(product);
+}
+getData();
+function drawTable(arr) {
+  tBody.innerHTML = "";
+  arr.forEach((element) => {
+    let tr = document.createElement("tr");
+    tr.innerHTML += `
      <td>${element.id}</td>
      <td>${element.name}</td>
      <td>${element.description}</td>
      <td><i class="fa-solid fa-trash onclick="deleteProduct(${element.id},this)"></i><i class="fa-solid fa-pen"></i></td>
           `;
-      tBody.append(tr);
-    });
-  }
-  async function deleteProduct(id, btn) {
-    try {
-      if (window.confirm("r u sure to delete??")) {
-        await axios.delete(`${BASE_URL}/cards/${id}`);
-        btn.closest(".box").remove();
-      }
-    } catch (error) {
-      console.log(error);
+    tBody.append(tr);
+  });
+}
+async function deleteProduct(id, btn) {
+  try {
+    if (window.confirm("r u sure to delete??")) {
+      await axios.delete(`${BASE_URL}/cards/${id}`);
+      btn.closest(".box").remove();
     }
+  } catch (error) {
+    console.log(error);
   }
-  
+}
+sort.addEventListener("click", function () {
+  let sorted;
+  if (sort.innerText === "asc") {
+    sort.innerText = "dec";
+    sorted = product.sort((a, b) => a.id - b.id);
+  } else if (sort.innerText === "dec") {
+    sort.innerText = "asc";
+    sorted = product.sort((a, b) => b.id - a.id);
+  } else if (sort.innerText === "asc") {
+    sorted = copyProduct;
+  }
+  drawTable(sorted);
+});
